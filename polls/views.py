cdb258ea8f14a970a78a1acf917c1f340ebbf88b
Mordechai Fast <mordechai.fast@gmail.com>
2022-02-12 23:50:08 +0200

    wrote first templates

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from django.template import loader
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, you are at the poll index.")
+    return render(request,'index.html',
+     {'latest_question_list': Question.objects.order_by('-pub_date')[:5]})
+
+def detail(request, question_id):
+    try:
+        return render(request, 'detail.html',
+         {'question': Question.objects.get(pk=question_id)})
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+def results(request, question_id):
+    return HttpResponse(f"The results of question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Voting on question {question_id}")
